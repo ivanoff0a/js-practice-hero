b2209cd138418711564de61fc8b1cb9dc6007ef3
ivanoff0a <ivanov-andrey15@mail.ru>
2018-02-01 18:09:58 +0300

    from home v.2

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 
-    var heroSpeech = "BRUUUUUUDAH";
+    var heroSpeech = "Hi, hope you're having a nice day!";
+    
     $(".js-showHide").click(function() {
-        $(".hero").toggleClass("-invisible")
+        $(".hero").toggleClass("-invisible");
     });
     $(".js-hello").click(function() {
-        $(".circle").text(heroSpeech);
+        $(".bubble").toggleClass("-visible");
+    });
+    $(".js-hello").click(function() {
+        $(".bubble").text(heroSpeech);
     });
 });
 
