e067bf9fb5fcf0645b87e67abb066f2dd1126125
ivanoff0a <ivanov-andrey15@mail.ru>
2018-02-05 17:36:29 +0300

    from home, add calculator v.4

--- script.js
+++ script.js
@@ -10,8 +10,29 @@ $(document).ready(function() {
         //     showBubble(inputText);
 
         // }
-        showBubble($(".js-text1").val() + $(".js-text2").val());
-    });
+        // showBubble($(".js-text1").val() + $(".js-text2").val());
+        
+        $(".js-hello").click( function () { // Событие нажатия на кнопку "Расчёт"
+		var action = $(".sign").val(); // Получаем значение действия, которое нужно выполнить
+		var first = $(".js-text1").val() * 1; // Переменная первого числа
+		var second = $(".js-text2").val() * 1; // Переменная второго числа
+		var result; // Переменная результата
+		if (action == '+') { // Если действие - сложение
+			result = first + second; //  складываем
+		}
+		else if (action == '-'){ // Если действие вычитание
+			result = first - second; // вычитаем
+		}
+		else if (action == '*'){ // Если действие умножение
+			result = first * second; // умножаем
+		}
+		else if (action == '/'){ // Если действие деления
+			result = first / second; // делим
+		}
+		$(".js-result").val(result); // записываем результат
+		// showBubble($(".bubble").val(result));
+		});
+   	});
 
     function showBubble(text) {
         $(".bubble").text(text);
