881683b67f41702a88c0a0db94a87a3ea1e193c1
bananaWeb <a89.volkov@gmail.com>
2018-02-08 21:02:01 +0300

    upgrade v.6

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
 
+    let hero = {
+        age: 20,
+        name: "XXXtentacion",
+        job: "Musician",
+
+        params: {
+            height: 180,
+            weight: 70,
+            gender: "Male",
+            race: "Afro American"
+        },
+
+        powers: {
+            mainPower: "Great Songs",
+            secondaryPower: "Run"
+        }
+    };
+
+    console.log(hero);
+
     $(".js-plus").click(function() {
         if (checkInputData().isNumber) {
             showBubble(Number(getFirstWord()) + Number(getSecondWord()));
         } else {
-            if (checkInputData().isEmpty) {
-                showBubble("Nothing in here");
-            } else {
-                showBubble(getFirstWord() + " " + getSecondWord());
-            }
+            showNotNumericData();
+        }
+
+        if (!checkInputData().isNumber) {
+            showBubble("Use numbers instead letters!");
         }
     });
 
@@ -16,11 +36,11 @@ $(document).ready(function() {
         if (checkInputData().isNumber) {
             showBubble(Number(getFirstWord()) - Number(getSecondWord()));
         } else {
-            if (checkInputData().isEmpty) {
-                showBubble("Nothing in here");
-            } else {
-                showBubble(getFirstWord() + " " + getSecondWord());
-            }
+            showNotNumericData();
+        }
+
+        if (!checkInputData().isNumber) {
+            showBubble("Use numbers instead letters!");
         }
     });
 
@@ -28,36 +48,44 @@ $(document).ready(function() {
         if (checkInputData().isNumber) {
             showBubble(Number(getFirstWord()) * Number(getSecondWord()));
         } else {
-            if (checkInputData().isEmpty) {
-                showBubble("Nothing in here");
-            } else {
-                showBubble(getFirstWord() + " " + getSecondWord());
-            }
+            showNotNumericData();
+        }
+
+        if (!checkInputData().isNumber) {
+            showBubble("Use numbers instead letters!");
         }
     });
 
     $(".js-divide").click(function() {
         if (checkInputData().isNumber) {
-            showBubble(Number(getFirstWord()) / Number(getSecondWord()));    
-        } else {
-            if (checkInputData().isEmpty) {
-                showBubble("Nothing in here");
+            if(Number(getSecondWord() === "0")) {
+                showBubble("What's wrong with you?");
             } else {
-                showBubble(getFirstWord() + " " + getSecondWord());
+                showBubble(Number(getFirstWord()) / Number(getSecondWord()));
             }
-
-        	// } else if (checkInputData().isNumeric(false)){
-         //        showBubble("Use numbers instead letters!");
-         //    }
+        } else {
+            showNotNumericData();
         }
 
-		if(Number(getSecondWord() === 0)) {
-        	showBubble("What's wrong with you?");
-   		}
+        if (!checkInputData().isNumber) {
+            showBubble("Use numbers instead letters!");
+        }
+     });
+
+    $(".js-object").click(function() {
+        console.log(hero.name);
+        showBubbleWithBlock('<div class="bubble-test"> Janseh Onfroy ' + hero.name + '!</div>');
     });
 
     // =================================================================
     // =================================================================
+    function showNotNumericData() {
+        if (checkInputData().isEmpty) {
+                showBubble("Nothing in here");
+            } else {
+                showBubble(getFirstWord() + " " + getSecondWord());
+            }
+    }
 
     function checkInputData() {
         // если буквы или пусто, то вывод пиши цифры
@@ -98,5 +126,24 @@ $(document).ready(function() {
         }, 2000);
     }
 
+    function showBubbleWithBlock(block) {
+        $(".bubble").append(block);
+        $(".bubble").addClass("-visible");
+
+        setTimeout(function() {
+            $(".bubble").removeClass("-visible");
+        }, 2000);
+    }
+
+    let movies = ["apple", "orange", "banana", "door"];
+
+    $(".js-array").click(function() {
+        movies.push(getSecondWord());
+        showBubble(movies);
+    });
+
+    for (let i = 0; i < movies.length; i++) {
+        $(".massive-panel").append('<div class="test-array">' + movies[i] + '</div>')
+    };
 });
 
